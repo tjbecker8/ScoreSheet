1b8932aa03e889ebbe167936444e353ad31cda9d
Thomas Becker <tjbecker@me.com>
2020-01-26 03:22:27 -0800

    still working on basic layout

--- src/components/GameSheet/gameSheet.js
+++ src/components/GameSheet/gameSheet.js
@@ -9,7 +9,89 @@ class GameSheet extends Component {
 
   render () {
     return (
-      <h1>hello world!?</h1>
+      <div>
+
+        <div>
+          <h1>team a Vs team b</h1>
+        </div>
+        <div>
+          <h3>league name</h3>
+        </div>
+        <div>
+          <h4>date and time</h4>
+        </div>
+
+        <div>
+          <h1> 0 - 0 </h1>
+        </div>
+
+        <div>
+          <h2>Goal team a</h2>
+        </div>
+
+        <div>
+          <h2>Goal taem b</h2>
+        </div>
+
+        <div>
+          <h3>save team a</h3>
+        </div>
+
+        <div>
+          <h3>save team b</h3>
+        </div>
+
+        <div>
+          <h2>Peanlty team a</h2>
+        </div>
+
+        <div>
+          <h2>Peanlty team b</h2>
+        </div>
+
+        <div>
+          <h3>Roster team a</h3>
+        </div>
+
+        <div>
+          <h3>roster team b</h3>
+        </div>
+
+        <div>
+          <h3>team a goalie</h3>
+          <h4>goalie stats</h4>
+        </div>
+
+        <div>
+          <h3>team b goalie</h3>
+          <h4>goalie stats</h4>
+        </div>
+
+        <div>
+          <h3>team a goals</h3>
+        </div>
+
+        <div>
+          <h3>team b goals</h3>
+        </div>
+
+        <div>
+          <h3>team a peanlties</h3>
+        </div>
+
+        <div>
+          <h3>team b peanlties</h3>
+        </div>
+
+        <div>
+          <h3>Game officials</h3>
+        </div>
+
+
+
+
+
+      </div>
     )
   }
 }
