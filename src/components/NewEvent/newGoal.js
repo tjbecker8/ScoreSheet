bddec3a68caab7074d16b88c937e71b3f0c6bed7
Thomas Becker <tjbecker@me.com>
2020-02-13 03:53:30 -0800

    adding dropdown menu

--- src/components/NewEvent/newGoal.js
+++ src/components/NewEvent/newGoal.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react';
 import './newGoal.css'
 
+import Select from "react-dropdown-select";
+
+const techCompanies = [
+  { label: "Apple", value: 1 },
+  { label: "Facebook", value: 2 },
+  { label: "Netflix", value: 3 },
+  { label: "Tesla", value: 4 },
+  { label: "Amazon", value: 5 },
+  { label: "Alphabet", value: 6 },
+];
+
 class NewGoal extends Component {
+
 
   render () {
     return(
@@ -9,6 +21,7 @@ class NewGoal extends Component {
         <p>Goal for Team A</p>
 
         <p>Scored By:</p>
+        <Select options={techCompanies} onChange={(values) => this.setValues(values)} />
 
         <p>Assists:</p>
 
