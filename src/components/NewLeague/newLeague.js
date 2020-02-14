735c4e09275f289e2473d51a1a797ad98ee4c00a
Thomas Becker <tjbecker@me.com>
2020-02-13 21:58:43 -0800

    firebase works

--- src/components/NewLeague/newLeague.js
+++ src/components/NewLeague/newLeague.js
@@ -6,6 +6,13 @@ import firebase from 'firebase'
 import "firebase/firestore"
 
 class NewLeague extends Component {
+  constructor(props){
+   super(props);
+   this.state = {
+     leagueName: ''
+   };
+ }
+
 
 createLeague = () => {
   var leagueRef = database.collection("leagues").doc()
@@ -17,15 +24,19 @@ createLeague = () => {
   console.log(leagueRef.id);
 }
 
-  render () {
+leagueNameSet = (e) => {
+  this.setState({leagueName: e.target.value})
+}
 
+  render () {
+    console.log(this.state.leagueName);
     return (
       <div className="newLeagueContainer">
         <p>New League</p>
 
         <div>
           <p>League Name</p>
-          <input type='text' />
+          <input type='text' onChange={(value)=>{this.leagueNameSet(value)}} />
         </div>
 
         <button type="button" onClick={this.createLeague} >Create League</button>
