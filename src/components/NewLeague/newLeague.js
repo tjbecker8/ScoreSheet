9ab5bdded89455bd063400e06ba9428cf7cfadf4
Thomas Becker <tjbecker@me.com>
2020-02-13 19:34:27 -0800

    setup for new league

---
+++ src/components/NewLeague/newLeague.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './newTeam.css'
+
+class NewLeague extends Component {
+
+  render () {
+    return (
+      <div>
+        <p>New League</p>
+      </div>
+    )
+  }
+}
