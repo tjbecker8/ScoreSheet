cfc8ba06bc49dc621a154fa7bcf445a0b6d46f78
Thomas Becker <tjbecker@me.com>
2020-02-13 16:37:33 -0800

    setup page for new player

---
+++ src/components/NewPlayer/newPlayer.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import './newPlayer.css'
+
+import Select from "react-dropdown-select";
+
+const techCompanies = [
+  { label: "Apple", value: 1 },
+  { label: "Facebook", value: 2 },
+  { label: "Netflix", value: 3 },
+  { label: "Tesla", value: 4 },
+  { label: "Amazon", value: 5 },
+  { label: "Alphabet", value: 6 },
+];
+
+
+class NewPlayer extends Component {
+
+  render () {
+    return (
+      <div>
+        <p>New Player</p>
+
+        <div>
+          <p>Team:</p>
+          <Select class="pointdropdown" options={techCompanies} onChange={(values) => this.setValues(values)} />
+        </div>
+
+        <div>
+          <p>First Name:</p>
+          <input type='text' />
+        </div>
+
+        <div>
+          <p>Last Name:</p>
+          <input type='text' />
+        </div>
+
+        <div>
+          <p>Number:</p>
+          <input type='text' />
+        </div>
+
+        <div>
+          <p>Position:</p>
+          <input type='text' />
+        </div>
+
+
+      </div>
+
+
+    )
+  }
+}
+
+export default NewPlayer
